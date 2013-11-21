f773b03d88eb5b049b89dadbee13f73ee277fbae
André Aubin <andre.aubin@lambdaweb.fr>
2013-11-21 18:38:28 +0100

    removing >80

--- libft.h
+++ libft.h
@@ -18,7 +18,7 @@
 
 void*	ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *s1, const void *s2, size_t n); /* FAIL (fix 096d2cd1f62c ?) */
+void	*ft_memcpy(void *s1, const void *s2, size_t n); /* (fix 096d2cd1f62c) */
 void	*ft_memccpy(void *s1, const void *s2, int c, size_t n);
 void	*ft_memmove(void *s1, const void *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
@@ -38,7 +38,8 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n);
 int	ft_strcmp(const char *s1, const char *s2);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
-int	ft_atoi(const char *str); /* Pourra etre factorisee avec isdigit. manque la gestion des espaces */
+int	ft_atoi(const char *str); /* Pourra etre factorisee avec isdigit.
+					manque la gestion des espaces */
 
 int     ft_isalpha(int c);
 int	ft_isdigit(int c);

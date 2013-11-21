3b448207555fc556846d270ab060842582d23143
André Aubin <andre.aubin@lambdaweb.fr>
2013-11-21 18:34:38 +0100

    cleaning up the lib header

--- libft.h
+++ libft.h
@@ -18,11 +18,11 @@
 
 void*	ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *s1, const void *s2, size_t n);// FAIL (fix 096d2cd1f62c ?)
+void	*ft_memcpy(void *s1, const void *s2, size_t n); /* FAIL (fix 096d2cd1f62c ?) */
 void	*ft_memccpy(void *s1, const void *s2, int c, size_t n);
 void	*ft_memmove(void *s1, const void *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
 
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
@@ -30,27 +30,25 @@ char	*ft_strcpy(char *s1, const char *s2);
 char	*ft_strncpy(char *s1, const char *s2, size_t n);
 char	*ft_strcat(char *s1, const char *s2);
 char	*ft_strncat(char *s1, const char *s2, size_t n);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);// FAIL
+size_t	ft_strlcat(char *dst, const char *src, size_t size); /* FAIL */
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-char	*ft_strstr(const char *s1, const char *s2);// Requires strncmp > ok
+char	*ft_strstr(const char *s1, const char *s2); /* Requires strncmp > ok */
 char	*ft_strnstr(const char *s1, const char *s2, size_t n);
-int		ft_strcmp(const char *s1, const char *s2);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int	ft_strcmp(const char *s1, const char *s2);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
-int		ft_atoi(const char *str); // Pourra etre factorisee avec isdigit. manque la gestion des espaces
+int	ft_atoi(const char *str); /* Pourra etre factorisee avec isdigit. manque la gestion des espaces */
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-int		ft_islower(int c); /* user */
-int		ft_isupper(int c); /* user */
+int     ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+int	ft_islower(int c); /* user */
+int	ft_isupper(int c); /* user */
 
-/*
-*/
 
 #endif

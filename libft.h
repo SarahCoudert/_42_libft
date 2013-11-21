2de0f10e73ce99b338c53e3a6d9f0cfed5561937
André Aubin <andre.aubin@lambdaweb.fr>
2013-11-21 06:41:45 +0100

    Adding memcmp + good tests

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aaubin <aaubin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/19 14:19:30 by aaubin            #+#    #+#             */
-/*   Updated: 2013/11/21 06:11:51 by aaubin           ###   ########.fr       */
+/*   Updated: 2013/11/21 06:32:39 by aaubin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void	*ft_memcpy(void *s1, const void *s2, size_t n);// FAIL (fix 096d2cd1f62c ?)
 void	*ft_memccpy(void *s1, const void *s2, int c, size_t n);
 void	*ft_memmove(void *s1, const void *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
@@ -50,7 +51,6 @@ int		ft_islower(int c); /* user */
 int		ft_isupper(int c); /* user */
 
 /*
-int		memcmp(const void *s1, const void *s2, size_t n);
 */
 
 #endif

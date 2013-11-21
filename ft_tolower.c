4c0929a40316b1c9a374b9c4ab7840d36b39f992
André Aubin <andre.aubin@lambdaweb.fr>
2013-11-21 21:25:02 +0100

    Fixing a (stupid) bug in to{upper,lower}()

--- ft_tolower.c
+++ ft_tolower.c
@@ -16,7 +16,7 @@ int		ft_tolower(int c)
 {
 	if (ft_isupper(c))
 	{
-		return (c - 32);
+		return (c + 32);
 	}
 	else
 	{
--- ft_toupper.c
+++ ft_toupper.c
@@ -16,7 +16,7 @@ int		ft_toupper(int c)
 {
 	if (ft_islower(c))
 	{
-		return (c + 32);
+		return (c - 32);
 	}
 	else
 	{

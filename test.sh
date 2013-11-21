36fdb50cd119c44ed69b977fec60969b0ebe6f67
André Aubin <andre.aubin@lambdaweb.fr>
2013-11-21 06:58:13 +0100

    Cleaning up all:

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-cd ../libTest
-make re
-cd ../libft
-cp ../libTest/libft_test.a .
-gcc Tests/ft_$1.test.c -L. -lft -lft_test -I . -o Tests/test_$1 -g -Wall -Wextra
-make re
-./Tests/test_$1
-rm -rf Tests/*.dSYM

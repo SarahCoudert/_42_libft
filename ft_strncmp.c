28e54bfb656b2b348481b3a9f1746624bfb7ee79
André Aubin <andre.aubin@lambdaweb.fr>
2013-11-25 22:55:06 +0100

    fix strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,13 +14,9 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int				diff;
-	unsigned int	counter;
-	int				stop;
+	unsigned int	len1;
+	unsigned int	len2;
 
-	diff = 0;
-	stop = 0;
-	counter = 0;
 #if defined(VERBOSE)
 	ft_putstr(">> ft_strncmp\n");
 	ft_putstr("\ts1 = [");
@@ -28,23 +24,20 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	ft_putstr("]\n\ts2 = [");
 	ft_putstr(s2);
 	ft_putstr("]\n\tsize = [");
-	ft_putstr(n);
-	ft_putstr("]\n")
+	ft_putstr("]\n");
 #endif
-	if ( s1 == NULL && s2 == NULL)
+
+	if ( s1 == NULL && s2 == NULL )
 		return (0);
-	else if (s1 == NULL || s2 == NULL)
-		return (999);
-	while (s1[counter] != '\0' && !stop)
-	{
-		diff += (s1[counter] - s2[counter]);
-		if (counter == n-1 || s2[counter] != s1[counter])
-			stop = 1;
-		counter++;
-	}
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	if ( n > len1 )
+		n = len1 + 1;
+	if ( n > len2 )
+		n = len2 + 1;
 
 #if defined(VERBOSE)
 	ft_putstr("<< ft_strncmp\n");
 #endif
-	return (diff);
+	return (ft_memcmp(s1, s2, n));
 }
